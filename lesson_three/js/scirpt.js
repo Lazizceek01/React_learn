b3ffb342527388a359b25d93b7d645a142fe0d0c
Laziz Azizov <azizovlaziz33@gmail.com>
2026-03-27 16:57:25 +0500

    lesson_3

---
+++ lesson_three/js/scirpt.js
@@ -0,0 +1,25 @@
+// Ozgaruvchilar 
+
+// 2 xil o'zgaruvchilar 
+let age = 22;
+const name = 'Laziz';
+
+console.log(age);
+console.log(name);
+
+const person = {
+	name: 'Laziz'
+};
+
+console.log(person);
+
+
+// O'zgaruvchilar 
+
+
+// Eski syntaxix 
+
+var fullName = "Laziz Azizov";
+console.log(fullName);
+
+
